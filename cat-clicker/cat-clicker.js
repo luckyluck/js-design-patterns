611f27449c64b6f5703575aad34389eeb7c05167
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-10-08 12:45:13 +0600

    Cat clicker improvements: add a sidebar to choose any cat from it as an active/current; add new cats.

--- cat-clicker/cat-clicker.js
+++ cat-clicker/cat-clicker.js
@@ -1,52 +1,90 @@
 window.onload = function () {
     let cats = [
-        {
-            name: 'Tom',
-            src: 'images/cat1.jpg'
-        },
-        {
-            name: 'Snow',
-            src: 'images/cat2.jpg'
-        }
+        { name: 'Tom', src: 'images/cat1.jpg', counter: 0 },
+        { name: 'Snow', src: 'images/cat2.jpg', counter: 0 },
+        { name: 'Puffy', src: 'images/cat3.jpg', counter: 0 },
+        { name: 'Smash', src: 'images/cat4.jpg', counter: 0 },
+        { name: 'Bug', src: 'images/cat5.jpg', counter: 0 }
     ];
 
-    for (let i = 0; i < cats.length; i++) {
-        catContainer(i, cats[i].name, cats[i].src);
-    }
+    createSidebar(cats);
+
+    document.querySelector('.catImg').addEventListener('click', function () {
+        let index = parseInt(this.getAttribute('data-index'), 10);
+        cats[index].counter++;
+        setContent('.counter', cats[index].counter);
+    });
+
+    // Setting first cat as a default
+    setMainImage(0, cats[0]);
 };
 
-function catContainer(index, catName, catImg) {
-    let clickCounter = 0;
-    // main container
-    let container = document.createElement('div');
-    container.setAttribute('class', 'catContainer');
-    // header with a cat name
-    let header = document.createElement('h6');
-    header.innerHTML = catName;
-    header.setAttribute('class', 'catName');
-    // cat image
-    let img = document.createElement('img');
-    img.id = `catImg_${index}`;
-    img.setAttribute('class', 'catImg');
-    img.src = catImg;
-    img.addEventListener('click', function () {
-        clickCounter++;
-
-        setContent(`#counter_${index}`, clickCounter);
-    });
-    // counter container
-    let counter = document.createElement('div');
-    counter.id = `counter_${index}`;
-    counter.setAttribute('class', 'counter');
-    counter.innerHTML = clickCounter + '';
-
-    container.appendChild(header);
-    container.appendChild(img);
-    container.appendChild(counter);
-
-    document.getElementById('main').appendChild(container);
+/**
+ * Generate sidebar
+ * @param cats
+ */
+function createSidebar(cats) {
+    let fragment = document.createDocumentFragment();
+    for (let i = 0; i < cats.length; i++) {
+        fragment.appendChild(getSidebarItem(cats[i], i));
+    }
+    document.getElementById('sidebar').appendChild(fragment);
 }
 
+/**
+ * Generate item Node
+ * @param cat
+ * @param index
+ * @returns {HTMLElement}
+ */
+function getSidebarItem(cat, index) {
+    let item = document.createElement('div');
+    item.setAttribute('class', 'sidebarItem');
+    let img = document.createElement('img');
+    img.src = cat.src;
+
+    item.appendChild(img);
+    item.addEventListener('click', function() {
+        setMainImage(index, cat);
+    });
+
+    return item;
+}
+
+/**
+ * Set innerHTML of the element with given selector
+ * @param selector
+ * @param content
+ */
 function setContent(selector, content) {
     document.querySelector(selector).innerHTML = content;
+}
+
+/**
+ * Setting active item by adding a class
+ * @param index - index of the selected item
+ */
+function setActiveItem(index) {
+    let items = document.querySelectorAll('.sidebarItem');
+    let activeItem = document.querySelector('.sidebarItem.active');
+
+    if (activeItem && activeItem === items[index]) {
+        return;
+    } else if (activeItem) {
+        activeItem.classList.remove('active');
+    }
+    items[index].classList.add('active');
+}
+
+/**
+ * Setting main image
+ * @param index
+ * @param cat
+ */
+function setMainImage(index, cat) {
+    setActiveItem(index);
+    document.querySelector('.catName').innerHTML = cat.name;
+    document.querySelector('.catImg').src = cat.src;
+    document.querySelector('.catImg').setAttribute('data-index', index);
+    document.querySelector('.counter').innerHTML = cat.counter;
 }
