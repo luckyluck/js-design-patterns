9b902f612458d8c0d6f331c0bb2069407eb8c21e
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-10-08 21:49:32 +0600

    Improve Cat Clicker to have something similar to MV* pattern.

--- cat-clicker/cat-clicker.js
+++ cat-clicker/cat-clicker.js
@@ -1,95 +1,89 @@
 window.onload = function () {
-    let cats = [
-        { name: 'Tom', src: 'images/cat1.jpg', counter: 0 },
-        { name: 'Snow', src: 'images/cat2.jpg', counter: 0 },
-        { name: 'Puffy', src: 'images/cat3.jpg', counter: 0 },
-        { name: 'Smash', src: 'images/cat4.jpg', counter: 0 },
-        { name: 'Bug', src: 'images/cat5.jpg', counter: 0 }
-    ];
+    // Model
+    let model = {
+        data: [
+            { name: 'Tom', src: 'images/cat1.jpg', counter: 0 },
+            { name: 'Snow', src: 'images/cat2.jpg', counter: 0 },
+            { name: 'Puffy', src: 'images/cat3.jpg', counter: 0 },
+            { name: 'Smash', src: 'images/cat4.jpg', counter: 0 },
+            { name: 'Bug', src: 'images/cat5.jpg', counter: 0 },
+            { name: 'Ginger', src: 'images/cat6.jpg', counter: 0 }
+        ],
+        getAllCats: function() {
+            return this.data;
+        },
+        getCat: function(index) {
+            return this.data[index];
+        },
+        updateCounter: function(index) {
+            this.data[index].counter += 1;
+        }
+    };
 
-    createSidebar(cats);
+    // Controller
+    let octopus = {
+        init: function() {
+            view.renderContent(0);
+        },
+        getCats: function() {
+            return model.getAllCats();
+        },
+        getCat: function(index){
+            return model.getCat(index) ;
+        },
+        updateCounter: function(index) {
+            model.updateCounter(index);
+            view.renderContent(index);
+        },
+        setCat: function (index) {
+            view.renderContent(index);
+        }
+    };
 
-    document.querySelector('.catImg').addEventListener('click', function () {
-        let index = parseInt(this.getAttribute('data-index'), 10);
-        cats[index].counter++;
-        setContent('.counter', cats[index].counter);
-    });
+    // View
+    let view = {
+        renderSidebar: function() {
+            let catList = document.querySelector('.cat-list');
+            catList.innerHTML = '';
 
-    // Setting first cat as a default
-    setMainImage(0, cats[0]);
+            let cats = octopus.getCats();
+            let template = document.querySelector('[id="sidebarItem"]').innerHTML;
+            let fragment = document.createDocumentFragment();
+
+            for (let i = 0; i < cats.length; i++) {
+                let li = document.createElement('li');
+                li.innerHTML = template.replace(/{{name}}/g, cats[i].name);
+                li.addEventListener('click', function () {
+                    octopus.setCat(i);
+                });
+                fragment.appendChild(li);
+            }
+
+            catList.appendChild(fragment);
+        },
+        renderContent: function(index) {
+            let catContainer = document.querySelector('.catContainer');
+            catContainer.innerHTML = '';
+            let cat = octopus.getCat(index);
+            if (!cat) {
+                return;
+            }
+
+            let template = document.querySelector('[id="catContainer"]').innerHTML;
+            template = template.replace(/{{name}}/g, cat.name);
+            template = template.replace(/{{src}}/g, cat.src);
+            template = template.replace(/{{counter}}/g, cat.counter);
+
+            catContainer.innerHTML = template;
+            catContainer.childNodes.item(3).addEventListener('click', function() {
+                octopus.updateCounter(index);
+            });
+        },
+        init: function() {
+            this.renderSidebar();
+        }
+    };
+
+    view.init();
+    octopus.init();
 };
-
-/**
- * Generate sidebar
- * @param cats
- */
-function createSidebar(cats) {
-    let fragment = document.createDocumentFragment();
-    for (let i = 0; i < cats.length; i++) {
-        fragment.appendChild(getSidebarItem(cats[i], i));
-    }
-    document.getElementById('sidebar').appendChild(fragment);
-}
-
-/**
- * Generate item Node
- * @param cat
- * @param index
- * @returns {HTMLElement}
- */
-function getSidebarItem(cat, index) {
-    let item = document.createElement('div');
-    item.setAttribute('class', 'sidebarItem');
-    let img = document.createElement('img');
-    img.src = cat.src;
-
-    let nameContainer = document.createElement('h6');
-    nameContainer.setAttribute('class', 'itemName');
-    nameContainer.innerHTML = cat.name;
-    nameContainer.addEventListener('click', function() {
-        setMainImage(index, cat);
-    });
-
-    item.appendChild(img);
-    item.appendChild(nameContainer);
-
-    return item;
-}
-
-/**
- * Set innerHTML of the element with given selector
- * @param selector
- * @param content
- */
-function setContent(selector, content) {
-    document.querySelector(selector).innerHTML = content;
-}
-
-/**
- * Setting active item by adding a class
- * @param index - index of the selected item
- */
-function setActiveItem(index) {
-    let items = document.querySelectorAll('.sidebarItem');
-    let activeItem = document.querySelector('.sidebarItem.active');
-
-    if (activeItem && activeItem === items[index]) {
-        return;
-    } else if (activeItem) {
-        activeItem.classList.remove('active');
-    }
-    items[index].classList.add('active');
-}
-
-/**
- * Setting main image
- * @param index
- * @param cat
- */
-function setMainImage(index, cat) {
-    setActiveItem(index);
-    document.querySelector('.catName').innerHTML = cat.name;
-    document.querySelector('.catImg').src = cat.src;
-    document.querySelector('.catImg').setAttribute('data-index', index);
-    document.querySelector('.counter').innerHTML = cat.counter;
-}
